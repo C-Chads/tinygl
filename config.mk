d1a3af46aec4aa092151fb978f4c20d3fff5e644
David <gek@katherine>
2021-02-23 14:18:59 -0500

    added mtune=native option to config mk

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@
 # C compiler
 
 CC= gcc
+#CFLAGS= -Wall -O3 -g -std=c99 -mtune=native -DNDEBUG
 CFLAGS= -Wall -O3 -g -std=c99 -march=native -DNDEBUG
 #CFLAGS= -Wall -O1 -g -std=c99 -Wno-undef -DNDEBUG
 LFLAGS=
