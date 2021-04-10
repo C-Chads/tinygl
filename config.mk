9dda1aa6621c073a9ac165b6c989c654bcb4b444
MHS <gek@katherine>
2021-04-10 18:52:57 -0400

    Automatic commit.

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@ CC= gcc
 #CFLAGS= -Wall -w -O3 -g -std=c99 -march=native -DNDEBUG
 
 #extract the maximum possible performance from gcc.
-CFLAGS= -Wall -Warray-bounds=2 -O3 -std=gnu99 -mtune=native -DNDEBUG -g -fopenmp
+CFLAGS= -Wall -Warray-bounds=2 -O3 -std=c99 -mtune=native -DNDEBUG -g -fopenmp
 #uncomment the following line for compiling the lib with tinyc
 #keep these flags for compiling with gcc if you are linking with tinygl compiled with tcc.
 #CFLAGS= -Wall -O3 -std=gnu99 -DNDEBUG -g
