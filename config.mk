5d3afc3312f2014ad3b054c36507c2c432e094e7
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-06 15:23:52 -0400

    Automatic commit.

--- config.mk
+++ config.mk
@@ -13,10 +13,14 @@ CC=cc
 #CFLAGS= -Wall -w -O3 -g -std=c99 -march=native -DNDEBUG
 
 #extract the maximum possible performance from gcc.
-CFLAGS= -Wall -O3 -std=c99 -DNDEBUG -march=native -Wno-unused-function
-#CFLAGS= -Wall -O3 -std=c99 -DNDEBUG -mtune=native -fopenmp -Wno-unused-function
-CFLAGS_LIB= -Wall -O3 -std=c99 -pedantic -DNDEBUG -march=native -Wno-unused-function
-#CFLAGS_LIB= -Wall -O3 -std=c99 -pedantic -DNDEBUG -fopenmp -mtune=native -Wno-unused-function
+
+
+#CFLAGS= -Wall -O3 -std=c99 -DNDEBUG -march=native -Wno-unused-function
+CFLAGS= -Wall -O3 -std=c99 -DNDEBUG -march=native -fopenmp -Wno-unused-function
+#CFLAGS_LIB= -Wall -O3 -std=c99 -pedantic -DNDEBUG -march=native -Wno-unused-function
+CFLAGS_LIB= -Wall -O3 -std=c99 -pedantic -DNDEBUG -fopenmp -march=native -Wno-unused-function
+
+
 #CFLAGS= -Wall -Ofast -std=gnu99 -DNDEBUG -mtune=native
 #uncomment the following line for compiling the lib with tinyc
 #keep these flags for compiling with gcc if you are linking with tinygl compiled with tcc.
